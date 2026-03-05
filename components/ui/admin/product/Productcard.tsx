e48792ce00d8d9dde6ea96bb3f629eb283e03251
amophi <nawwhod@gmail.com>
2026-03-05 23:57:48 +0900

    fix: added react to produtcard

--- components/ui/admin/product/Productcard.tsx
+++ components/ui/admin/product/Productcard.tsx
@@ -1,4 +1,5 @@
-﻿import Image from 'next/image';
+﻿import React from 'react';
+import Image from 'next/image';
 import CheckboxButton from '@/components/ui/button/CheckboxButton';
 import ToggleSwitch from '@/components/ui/button/ToggleSwitch';
 import ProductLike from './ProductLike';
