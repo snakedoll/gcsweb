ae3f89a0aac355371479bc0531cfe40af77b370f
amophi <nawwhod@gmail.com>
2026-08-16 00:23:51 +0900

    fix(admin-team): 전화번호 포맷팅 함수 임포트 수정

--- app/admin/team/[id]/edit/page.tsx
+++ app/admin/team/[id]/edit/page.tsx
@@ -8,7 +8,7 @@ import Button from "@/components/ui/button/Button";
 import RadioButton from "@/components/ui/button/RadioButton";
 import { useSession } from "next-auth/react";
 import Image from "next/image";
-import { formatPhoneWithHyphen } from "@/lib/format-phone";
+import { formatPhoneWithHyphen as formatPhone } from "@/lib/format-phone";
 
 // local icons
 const CROWN_ICON = "/assets/icons/additional/tabler_crown.svg";
